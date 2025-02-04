b54d960f591594e0dfe6dd4d09173e06d780ad90
Fersh5 <dev_fer5@outlook.com>
2025-02-03 22:56:06 -0600

    add features, readme

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-Un buen README incluye información sobre la instalación, dependencias, cómo ejecutar el proyecto, y posibles problemas o soluciones.
+<!-- Un buen README incluye información sobre la instalación, dependencias, cómo ejecutar el proyecto, y posibles problemas o soluciones. -->
+## Amigo Secreto
+Challenge del programa **One Oracle Next Education** de la plataforma AluraLatam
 
-Detalla sus funcionalidades, incluyendo capturas de pantalla o videos que muestren cómo agregar nombres y realizar el sorteo. Esta práctica mejorará la documentación, y hará que tu proyecto sea más accesible y profesional para la comunidad.
+Con el objetivo de practicar y consolidar los conocimientos de los cursos de lógica de programación con JavaScript.
 
+### Funcionalidades:
+* Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar".
+
+* Validar entrada: Si el campo de texto está vacío, o no cumple con los criterios de ser un nombre correcto, el programa mostrará una alerta pidiendo un nombre válido.
+
+* Valida que no existan duplicados, en caso de intentar el mismo nombre se indicará a través de una alerta.
+
+* Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
+
+* Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página.
+
+![Pagina de inicio](assets/image.png)
--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+let friends = [];
+
+function addFriend(){
+    let input = document.querySelector('#amigo');
+    if (verifyData(input)){
+        friends.push(capitalize(input.value));
+        showListFriends();
+    }
+    input.value = ''; 
+}
+
+function verifyData(input){
+    let validate = input.value.trim();
+    let regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+    if (input.value == null || input.value.length == 0){
+        alert('Por favor, ingresa un nombre.');
+        return false;
+    } else if (!regex.test(validate)){
+        alert('Por favor, ingresa un nombre valido.');
+        return false;
+    } else if (friends.includes(capitalize(input.value))){
+        alert('Ya añadiste a este amigo.');
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function showListFriends(){
+    let lista = document.querySelector('#listaAmigos');
+    lista.innerHTML = '';
+    for (let i=0; i<friends.length;i++){
+        let newElement = document.createElement('li');
+        newElement.textContent=friends[i];
+        lista.appendChild(newElement);
+    }
+}
+
+function secretFriend(){
+    if (friends.length){
+        let result = document.querySelector('#resultado');
+        result.innerHTML='';
+        indexFriend= Math.floor(Math.random()*friends.length);
+        let friend = document.createElement('li');
+        friend.textContent=friends[indexFriend];
+        result.appendChild(friend);
+    }else { 
+        alert('Aún no ingresas ningun nombre.');
+    }
+}
+
+function capitalize(nombre) {
+    return nombre.toLowerCase().replace(/\b\w/g, letra => letra.toUpperCase());
+}
